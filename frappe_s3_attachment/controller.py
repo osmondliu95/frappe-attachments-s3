77e418bfbd3aa31f78cc7431122772c3ad815ba1
osmondliu95 <44262236+osmondliu95@users.noreply.github.com>
2021-12-20 21:46:39 +0800

    Update controller.py

--- frappe_s3_attachment/controller.py
+++ frappe_s3_attachment/controller.py
@@ -32,7 +32,7 @@ class S3Operations(object):
                                            region_name=self.s3_settings_doc.region_name,
                                            aws_access_key_id=self.s3_settings_doc.aws_key,
                                            aws_secret_access_key=self.s3_settings_doc.aws_secret,
-                                           endpoint_url='https://oss-cn-shanghai.aliyuncs.com',
+                                           endpoint_url='https://'+self.s3_settings_doc.region_name+'.aliyuncs.com',
                                            config=Config(s3={"addressing_style": "virtual"})
                                          )
         else:
@@ -90,10 +90,9 @@ class S3Operations(object):
 
         if not doc_path:
             if self.folder_name:
-                 final_key = '2019010101010'
-#                final_key = self.folder_name + "/" + year + "/" + month + \
-#                    "/" + day + "/" + parent_doctype + "/" + key + "_" + \
-#                    file_name
+                 final_key = self.folder_name + "/" + year + "/" + month + \
+                   "/" + day + "/" + parent_doctype + "/" + key + "_" + \
+                   file_name
             else:
                 final_key = year + "/" + month + "/" + day + "/" + \
                     parent_doctype + "/" + key + "_" + file_name
